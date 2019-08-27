bf983be267ef8bc9c51cf750c92c415d23d94374
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-27 10:07:36 +1000

    Added UPD_PROD_SALESYTD.qsl

---
+++ UPD_PROD_SALESYTD.sql
@@ -0,0 +1,38 @@
+DROP PROCEDURE IF EXISTS UPD_PROD_SALESYTD;
+GO
+
+CREATE PROCEDURE UPD_PROD_SALESYTD @PPRODID int, @PAMT INT AS
+
+BEGIN
+    BEGIN TRY
+
+        IF (@PPRODID IS NULL)
+            THROW 50100, 'Product ID not found.', 1
+        ELSE IF @PAMT < -999.99 OR @PAMT > 999.99
+            THROW 50110, 'Amount out of range.', 1
+
+        UPDATE PRODUCT
+            SET SALES_YTD = @PAMT
+            WHERE PRODID = @PPRODID
+    END TRY
+    BEGIN CATCH
+
+        IF ERROR_NUMBER() = 50100
+            THROW
+        ELSE IF ERROR_NUMBER() = 50110
+            THROW
+        ELSE
+            BEGIN
+                DECLARE @ERRORMESSAGE NVARCHAR(MAX) = ERROR_MESSAGE();
+                THROW 50000, @ERRORMESSAGE, 1
+            END;
+    END CATCH;
+
+END;
+GO
+
+SELECT * FROM PRODUCT;
+
+EXEC UPD_PROD_SALESYTD @PPRODID = 1500, @PAMT = 250;
+
+SELECT * FROM PRODUCT;
